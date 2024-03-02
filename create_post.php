84ebab9115eaba7400ee6c5f23fff5e8eef9786b
Eric Smallwood <smallwood.eric@ymail.com>
2024-03-02 11:11:33 +0800

    basic security added

--- create_post.php
+++ create_post.php
@@ -9,15 +9,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $title = $data['title'];
         $body = $data['body'];
 
-        try {
-            $data = json_decode($json, true);
-            $sql = "INSERT INTO posts (title, body) VALUES (:title, :body)";
-            error_log($sql);
-            $stmt = $conn->prepare($sql);
-            $stmt->execute(['title' => $title, 'body' => $body]);
-            echo "Post successful!";
-        } catch (Exception $e) {
-            echo 'Caught exception: ',  $e->getMessage(), "\n";
+        if(strlen($title) > 255) {
+          echo "Title Too Large";
+        } else if(str_word_count($body) > 1000 || strlen($body) > 8000) {
+            echo "Body Too Large";
+        } else {
+            try {
+                $data = json_decode($json, true);
+                $sql = "INSERT INTO posts (title, body) VALUES (:title, :body)";
+                error_log($sql);
+                $stmt = $conn->prepare($sql);
+                $stmt->execute(['title' => $title, 'body' => $body]);
+                echo "Post successful!";
+            } catch (Exception $e) {
+                echo 'Caught exception: ',  $e->getMessage(), "\n";
+            }
         }
     } else {
         echo "Title and body are required.";
--- index.js
+++ index.js
@@ -20,13 +20,25 @@ jQuery(function() {
             body: jQuery('#postBody').val()
         };
 
+        if(postData.title.length > 255) {
+            return alert("Title length cannot exceed 255 characters")
+        }
+
+        if(postData.body.split(" ").length > 1000) {
+            return alert("Body length cannot exceed 1000 words")
+        }
+
+        if(postData.body.length > 8000) {
+            return alert("Body length cannot exceed 8000 characters")
+        }
+
         jQuery.ajax({
             url: 'create_post.php',
             type: 'POST',
             contentType: 'application/json',
             data: JSON.stringify(postData),
             success: function(result) {
-                alert('Post successfully created!');
+                alert(result);
                 jQuery('#postTitle').val('');
                 jQuery('#postBody').val('');
                 fetchPosts();
